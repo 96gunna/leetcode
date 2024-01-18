32af47dbb0dedce617fb8f6e9885e50b388d5d85
Alfredo Gunn <98775340+96gunna@users.noreply.github.com>
2024-01-18 15:29:11 -0800

    167. Two Sum II - Input Array Is Sorted

---
+++ Java/167twoSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // Initialize left pointer to beginning index
+        int left = 0;
+        // Initialize right pointer to ending index
+        int right = numbers.length - 1;
+        while (left < right) {
+            // Calculate the current sum
+            int currSum = numbers[left] + numbers[right];
+            // If the target is found return the pointers+1 since question wants base 1
+            if (currSum == target) return new int[]{left+1, right+1};
+            // If currSum is larger than the target decrement the right
+            if (currSum > target) right--;
+            // If currSum is smaller than the target increment the left
+            else left++;
+        }
+        // This will never happen since a solution is guaranteed
+        return null;
+    }
+}
